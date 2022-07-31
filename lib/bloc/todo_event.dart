aeafba5074f720947090247f726e8c8badda781a
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-08-01 01:39:06 +0300

    Added TodoEvents

--- lib/bloc/todo_event.dart
+++ lib/bloc/todo_event.dart
@@ -2,3 +2,11 @@ part of 'todo_bloc.dart';
 
 @immutable
 abstract class TodoEvent {}
+
+class GetTodoEvent extends TodoEvent{}
+
+class AddTodoEvent extends TodoEvent{
+  final Todo todo;
+
+  AddTodoEvent({required this.todo});
+}
