140e3e9ae8fa3b24dba8ab265eac301d41ee7970
Ecem Bostancıoğlu <ecembostancioglu21@gmail.com>
2022-07-31 17:09:58 +0300

    Created HomeView design

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -5,6 +5,67 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+        body: SafeArea(
+          child: CustomScrollView(
+             slivers: [
+               SliverAppBar(
+                 actions: [
+                   Padding(
+                     padding: EdgeInsets.only(right:10,top: 10),
+                     child: IconButton(
+                         onPressed:(){},
+                         icon: Icon(Icons.add,
+                           color: Colors.purple.shade600,
+                         size: 40)),
+                   )
+                 ],
+                 backgroundColor: Colors.transparent,
+                expandedHeight: 250.0,
+                 flexibleSpace: FlexibleSpaceBar(
+                    background: Image.asset('assets/images/good_vibes.jpg',
+                  fit: BoxFit.fitWidth),
+             ),
+            ),
+               SliverList(
+                 delegate: SliverChildBuilderDelegate(
+                       (_, int index) {
+                     return ListTile(
+                       leading: Container(
+                           padding: EdgeInsets.all(8),
+                           width: 100),
+                       title: Text('Good Vibes Only'),
+                     );
+                   },
+                   childCount: 20,
+                 ),
+               ),
+          ]),
+        ),
+    );
   }
 }
+
+//CustomScrollView(
+//           slivers: [
+//             SliverAppBar(
+//               toolbarHeight:250,
+//               flexibleSpace: FlexibleSpaceBar(
+//                 background: Image.asset('assets/images/good_vibes.jpg',
+//                     fit: BoxFit.cover),
+//               ),
+//             ),
+//             SliverToBoxAdapter(
+//               child: ListView.builder(
+//                 physics:AlwaysScrollableScrollPhysics(),
+//                   shrinkWrap: true,
+//                   itemCount: 16,
+//                   itemBuilder: (context,index){
+//                     return ListTile(
+//                       title: Text('Good Vibes Only'),
+//                     );
+//                   }),
+//             ),
+//           ],
+//         ),
